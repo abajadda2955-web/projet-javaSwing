d78766dafc0f08a7c1875721621c5707488cf101
SALMA BENBOUKHRIS <salmabenboukhris@gmail.com>
2026-03-16 14:20:19 +0000

    ajout de model

--- src/convertisseur/model/DistanceConverter.java
+++ src/convertisseur/model/DistanceConverter.java
@@ -1,4 +1,26 @@
 package convertisseur.model;
 
 public class DistanceConverter {
+
+    public static double convertir(double valeur, String de, String vers){
+
+        double metre;
+
+        if(de.equals("Kilometre"))
+            metre = valeur * 1000;
+
+        else if(de.equals("Mile"))
+            metre = valeur * 1609.34;
+
+        else
+            metre = valeur;
+
+        if(vers.equals("Kilometre"))
+            return metre / 1000;
+
+        if(vers.equals("Mile"))
+            return metre / 1609.34;
+
+        return metre;
+    }
 }
--- src/convertisseur/model/PoidsConverter.java
+++ src/convertisseur/model/PoidsConverter.java
@@ -1,4 +1,26 @@
 package convertisseur.model;
 
 public class PoidsConverter {
+
+    public static double convertir(double valeur, String de, String vers){
+
+        double gramme;
+
+        if(de.equals("Kilogramme"))
+            gramme = valeur * 1000;
+
+        else if(de.equals("Livre"))
+            gramme = valeur * 453.59;
+
+        else
+            gramme = valeur;
+
+        if(vers.equals("Kilogramme"))
+            return gramme / 1000;
+
+        if(vers.equals("Livre"))
+            return gramme / 453.59;
+
+        return gramme;
+    }
 }
